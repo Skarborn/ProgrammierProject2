05569134c3984b158f7971f73f4c7c9ebe86d242
Martin Berdau <martinberdau@student.jade-hs.de>
2019-06-01 16:36:54 +0200

    Funktionen wurde nerstellt, welche spaeter zum Widerherstellen genutzt werden

--- Restore.py
+++ Restore.py
@@ -3,7 +3,12 @@ import pathlib
 def get_filelength(file):
     length = int.from_bytes(file.read(4),"little")
     return length
-    
+
+def restore_wave(file):
+    pass
+
+def restore_avi(file):
+    pass
 
 absolutePath = '/Users/martinberdau/Desktop/data_deleted.img'
 
@@ -22,5 +27,6 @@ with disk.open('rb') as file:
                         file_type = file.read(4)
                         if file_type==b'WAVE':
                             print("WAVE-Datei")
+                            restore_wave(file)
                         if file_type==b'AVI ':
                             print("AVI-Datei")
