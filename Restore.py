b3eb5f9690573c83c13d1c15eeba51ec63200df0
Martin Berdau <martinberdau@student.jade-hs.de>
2019-06-08 19:06:55 +0200

    Effektivere Suche nach Headern. Eine weitere avi kann wiederhergestellt werden

--- Restore.py
+++ Restore.py
@@ -23,20 +23,21 @@ def restore_avi(file,file_length,found_avis):
 
 # FIND AND RESTORE JPEGS
 def restore_JPEG(file,found_jpegs):
-	new_JPEG = pathlib.Path(f"restored_jpeg_{found_jpegs+1}.jpeg")
-	start_img = 1
-	current = file.read(1)
-	current_before = b'0'
-	with new_JPEG.open('wb') as new_file:
-		new_file.write(b'\xFF\xD8')
-		while start_img > 0:
-			current_before = current
-			current = file.read(1)
-			new_file.write(current)
-			if current_before+current == b'\xFF\xD8':
-				start_img += 1
-			if current_before+current == b'\xFF\xD9':
-				start_img -= 1
+	pass
+	# new_JPEG = pathlib.Path(f"restored_jpeg_{found_jpegs+1}.jpeg")
+	# start_img = 1
+	# current = file.read(1)
+	# current_before = b'0'
+	# with new_JPEG.open('wb') as new_file:
+	# 	new_file.write(b'\xFF\xD8')
+	# 	while start_img > 0:
+	# 		current_before = current
+	# 		current = file.read(1)
+	# 		new_file.write(current)
+	# 		if current_before+current == b'\xFF\xD8':
+	# 			start_img += 1
+	# 		if current_before+current == b'\xFF\xD9':
+	# 			start_img -= 1
 
 
 
@@ -47,29 +48,35 @@ disk = pathlib.Path(absolutePath)
 disk_size = disk.stat()[6]
 
 
-# counts for files
-found_wavs = 0
-found_avis = 0
-found_jpegs = 0
-
 with disk.open('rb') as file:
-##    for x in range(disk_size):
-##        if file.read(4) == b'RIFF':
-##            print("RIFF gefunden")
-##            file_length = int.from_bytes(file.read(4),"little")
-##            riff_type = file.read(4)
-##            if riff_type == b'WAVE':
-##                print("WAVE-Datei")
-##                restore_wave(file,file_length,found_wavs)
-##                found_wavs += 1
-##            if riff_type == b'AVI ':
-##                print("AVI-Datei")
-##                restore_avi(file,file_length,found_avis)
-##                found_avis += 1
-                
-    for x in range(disk_size):            
-    	if file.read(2) == b'\xFF\xD8':
-    		JPEG_type = file.read(2)
-    		if JPEG_type == b'\xFF\xE0':
-        		restore_JPEG(file,found_jpegs)
-        		found_jpegs += 1
+
+	# initial 4 bytes that will be looked at to find header
+	b3 = file.read(1)
+	b2 = file.read(1)
+	b1 = file.read(1)
+	b0 = file.read(1)
+
+	for x in range(disk_size - 4):
+		if b1+b0 == b'\xFF\xD8':
+			JPEG_type = file.read(2)
+			if JPEG_type == b'\xFF\xE0':
+				restore_JPEG(file,found_jpegs)
+				found_jpegs += 1
+
+		if b3+b2+b1+b0 == b'RIFF':
+			print("RIFF gefunden")
+			file_length = int.from_bytes(file.read(4),"little")
+			riff_type = file.read(4)
+			if riff_type == b'WAVE':
+				print("WAVE-Datei")
+				restore_wave(file,file_length,found_wavs)
+				found_wavs += 1
+			if riff_type == b'AVI ':
+				print("AVI-Datei")
+				restore_avi(file,file_length,found_avis)
+				found_avis += 1
+
+		b3 = b2
+		b2 = b1
+		b1 = b0
+		b0 = file.read(1)
